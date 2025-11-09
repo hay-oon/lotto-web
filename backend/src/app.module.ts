5298197723ade2759c3af39a552edf84d9d92f24
hay-oon <hayooncode@gmail.com>
2025-11-09 22:40:10 +0900

    feat: LottoModule 추가
    - LottoModule 생성 및 AppModule에 등록

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LottoModule } from './lotto/lotto.module';
 
 @Module({
-  imports: [],
+  imports: [LottoModule],
   controllers: [AppController],
   providers: [AppService],
 })
